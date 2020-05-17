f0256b08fdc79e2ae6a8016be32821d5fed7e4fe
G1ovann16 <glandaburo92@gmail.com>
2020-05-17 17:47:37 +0200

    add model backend category

---
+++ backend/models/Category.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+const CategorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    }
+});
+const Category = mongoose.model('Category', CategorySchema);
+export default Category;
