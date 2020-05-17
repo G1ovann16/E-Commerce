97908a2691d44f235e5a1733dc549025d86dbd41
G1ovann16 <glandaburo92@gmail.com>
2020-05-17 17:45:34 +0200

    add config backend key

---
+++ backend/config/key.js
@@ -0,0 +1,17 @@
+// const config = {
+//     SECRET: "tiib!41$Pe5ef#-L$?lxL3#LVYcWftpc^Lqi0txX6!CvUQR_^KBqb6i!oa!MtgQZ$VS2MFH|-JV0oHFoULqi0txX6!CvUQR_^KBqb6i!oa!MtgQZ$VS2MFH|-$jKH+Qb#Nft?syXT9fDQAo1vgb3cV?_G&36GfT2P$EOMULby7Vhx0#nMz4+YPPug_C41enRTDTRsR5LPMq2vjpmF8kx?GGUN^ufaF_8t4Eh8HuD4Tu-+@OazSMx9^7SI5PKet_?8Is2R_Tn?=4|8+!SyY7zaoRU-Ss9YL5AhA5-9BJ61+hmkP9UBZjK*6?-9ecGHF7&%V35L3@G6Tu$0Li?8BaU%VbsN$&6-l0XeAPsY8s&8$BmTK^3tRTsi=7+F^+rbBlJ*N3zM^tnraKuMAH|-JV0oHFoULqi0txX6!CvUQR_^KBqb6i!oa!MtgQZ$VS2MFH|-$jKH+Qb#Nft?syXT9fDQAo1vgb3cV?_G&36GfT2P$EOMULby7Vhx0#nMz4+YPPug_C41enRTDTRsR5LPMq2vjpmF8kx?GGUN^ufaF_8t4Eh8HuD4Tu-+@OazSMx9^7SI5PKet_?8Is2R_Tn?=4|8+!SyY7zaoRU-Ss9YL5AhA5-9BJ61+hmkP9UBZjK*6?-9ecGHF7&%V35L3@G6Tu$0Li?8BaU%VbsN$&6-l0XeAPsY8s&8$BmTK^e%Th1&dIJ7R+|s_xJV4sGHWXjRa=8o0pkTpS8Pe%^kV2sD@c6APxsbzZEykyJ*",
+//     GMAIL: {
+//         email: 'bootcampstream@gmail.com',
+//         password: '123bootcamp123'
+//     },
+//     EMAIL_SECRET: "8e4fw838f4se84f8ss65",
+//     SESSION_SECRET: "g89511d8g18SsEK1f8"
+// };
+
+// module.exports = config;
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
